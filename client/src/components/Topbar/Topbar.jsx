0762ede97dbb2a371f38955c28bb0eb749e15545
KawsarCoder <md.kawsar.developer@gmail.com>
2024-01-02 12:54:08 +0600

    top navbar responsive

--- client/src/components/Topbar/Topbar.jsx
+++ client/src/components/Topbar/Topbar.jsx
@@ -155,24 +155,13 @@ const Topbar = () => {
 
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 "
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 -ml-40"
             >
               <li>
                 <a>Item 1</a>
               </li>
               <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
+                <a>Item 2</a>
               </li>
             </ul>
           </div>

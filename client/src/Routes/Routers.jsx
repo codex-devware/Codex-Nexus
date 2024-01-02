015826c7e79803b6817e1e581c7739db476688c7
ikhtiarbabor <ikhtiarbabor@gmail.com>
2024-01-02 09:36:30 +0600

    remove shadcn

--- client/src/Routes/Routers.jsx
+++ client/src/Routes/Routers.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "../Pages/Home/Home";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from '../Pages/Home/Home';
 
 const Routers = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path='/' element={<Home />} />
+          
         </Routes>
       </Router>
     </>

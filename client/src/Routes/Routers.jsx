4f789a2c32c823d48cba79888d9e90b2d56280a6
KawsarCoder <md.kawsar.developer@gmail.com>
2024-01-02 12:42:46 +0600

    top navbar structure created

--- client/src/Routes/Routers.jsx
+++ client/src/Routes/Routers.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '../Pages/Home/Home';
-import { Test } from '@/components/Home/Test';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import { Test } from "@/components/Home/Test";
+import Navbar from "../components/Navbar/Navbar";
 
 const Routers = () => {
   return (
     <>
       <Router>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/test' element={<Test />} />
-
-          
+          <Route path="/" element={<Home />} />
+          <Route path="/test" element={<Test />} />
         </Routes>
       </Router>
     </>

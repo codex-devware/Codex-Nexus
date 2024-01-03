113ee8e34757aea82cf0490d02e30182aa93a3a9
ikhtiarbabor <ikhtiarbabor@gmail.com>
2024-01-03 07:53:05 +0600

    cloning

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -2,18 +2,18 @@ const Sidebar = () => {
   return (
     <section>
       <div>
-        <h1 className="text-2xl font-bold text-center mt-6 underline text-blue-500">
+        <h1 className='text-2xl font-bold text-center mt-6 underline text-blue-500'>
           Codex Devware
         </h1>
-        <div className="m-6 font-semibold text-md">
+        <div className='m-6 font-semibold text-md '>
           <div
             tabIndex={0}
-            className="collapse collapse-arrow border-none -mb-6"
+            className='collapse collapse-arrow border-none -mb-6'
           >
-            <div className="collapse-title text-md font-semibold">
+            <div className='collapse-title text-md font-semibold'>
               Sales Overview
             </div>
-            <div className="collapse-content">
+            <div className='collapse-content'>
               <ul>
                 <li>Total Sales</li>
                 <li>Net Sales</li>
@@ -22,11 +22,11 @@ const Sidebar = () => {
               </ul>
             </div>
           </div>
-          <div tabIndex={1} className="collapse collapse-arrow border-none">
-            <div className="collapse-title text-md font-semibold">
+          <div tabIndex={1} className='collapse collapse-arrow border-none'>
+            <div className='collapse-title text-md font-semibold'>
               Order Management
             </div>
-            <div className="collapse-content">
+            <div className='collapse-content'>
               <ul>
                 <li>Pending Orders</li>
                 <li>Completed Orders</li>
@@ -36,11 +36,11 @@ const Sidebar = () => {
               </ul>
             </div>
           </div>
-          <div tabIndex={2} className="collapse collapse-arrow border-none">
-            <div className="collapse-title text-md font-semibold">
+          <div tabIndex={2} className='collapse collapse-arrow border-none'>
+            <div className='collapse-title text-md font-semibold'>
               Customer Insights
             </div>
-            <div className="collapse-content">
+            <div className='collapse-content'>
               <ul>
                 <li>Top-selling products</li>
                 <li>Low-selling products</li>
@@ -49,22 +49,22 @@ const Sidebar = () => {
               </ul>
             </div>
           </div>
-          <div tabIndex={3} className="collapse collapse-arrow border-none">
-            <div className="collapse-title text-md font-semibold">
+          <div tabIndex={3} className='collapse collapse-arrow border-none'>
+            <div className='collapse-title text-md font-semibold'>
               Product Performance
             </div>
-            <div className="collapse-content">
+            <div className='collapse-content'>
               <ul>
                 <li>Total Sales</li>
                 <li>Net Sales</li>
               </ul>
             </div>
           </div>
-          <div tabIndex={4} className="collapse collapse-arrow border-none">
-            <div className="collapse-title text-md font-semibold">
+          <div tabIndex={4} className='collapse collapse-arrow border-none'>
+            <div className='collapse-title text-md font-semibold'>
               Revenue Analytics
             </div>
-            <div className="collapse-content">
+            <div className='collapse-content'>
               <ul>
                 <li>Revenue by date range</li>
                 <li>Revenue by product category</li>
@@ -73,11 +73,11 @@ const Sidebar = () => {
               </ul>
             </div>
           </div>
-          <div tabIndex={5} className="collapse collapse-arrow border-none">
-            <div className="collapse-title text-md font-semibold">
+          <div tabIndex={5} className='collapse collapse-arrow border-none'>
+            <div className='collapse-title text-md font-semibold'>
               Shipping and Tax
             </div>
-            <div className="collapse-content">
+            <div className='collapse-content'>
               <ul>
                 <li>Shipping costs</li>
                 <li>Tax collection</li>
@@ -85,11 +85,11 @@ const Sidebar = () => {
               </ul>
             </div>
           </div>
-          <div tabIndex={6} className="collapse collapse-arrow border-none">
-            <div className="collapse-title text-md font-semibold">
+          <div tabIndex={6} className='collapse collapse-arrow border-none'>
+            <div className='collapse-title text-md font-semibold'>
               Coupon and Discount Analytics
             </div>
-            <div className="collapse-content">
+            <div className='collapse-content'>
               <ul>
                 <li>Total coupons used</li>
                 <li>Coupon effectiveness</li>
@@ -97,11 +97,11 @@ const Sidebar = () => {
               </ul>
             </div>
           </div>
-          <div tabIndex={7} className="collapse collapse-arrow border-none">
-            <div className="collapse-title text-md font-semibold">
+          <div tabIndex={7} className='collapse collapse-arrow border-none'>
+            <div className='collapse-title text-md font-semibold'>
               Stock and Inventory Management
             </div>
-            <div className="collapse-content">
+            <div className='collapse-content'>
               <ul>
                 <li>Low stock alerts</li>
                 <li>Out of stock products</li>
@@ -109,11 +109,11 @@ const Sidebar = () => {
               </ul>
             </div>
           </div>
-          <div tabIndex={8} className="collapse collapse-arrow border-none">
-            <div className="collapse-title text-md font-semibold">
+          <div tabIndex={8} className='collapse collapse-arrow border-none'>
+            <div className='collapse-title text-md font-semibold'>
               Refund and Returns
             </div>
-            <div className="collapse-content">
+            <div className='collapse-content'>
               <ul>
                 <li>Refund requests</li>
                 <li>Return status</li>
@@ -121,11 +121,11 @@ const Sidebar = () => {
               </ul>
             </div>
           </div>
-          <div tabIndex={9} className="collapse collapse-arrow border-none">
-            <div className="collapse-title text-md font-semibold">
+          <div tabIndex={9} className='collapse collapse-arrow border-none'>
+            <div className='collapse-title text-md font-semibold'>
               Performance Metrics
             </div>
-            <div className="collapse-content">
+            <div className='collapse-content'>
               <ul>
                 <li>Pageviews</li>
                 <li>Conversion rate</li>
@@ -134,11 +134,11 @@ const Sidebar = () => {
               </ul>
             </div>
           </div>
-          <div tabIndex={10} className="collapse collapse-arrow border-none">
-            <div className="collapse-title text-md font-semibold">
+          <div tabIndex={10} className='collapse collapse-arrow border-none'>
+            <div className='collapse-title text-md font-semibold'>
               User Management
             </div>
-            <div className="collapse-content">
+            <div className='collapse-content'>
               <ul>
                 <li>Admin and user roles</li>
                 <li>User activity log</li>
@@ -146,11 +146,11 @@ const Sidebar = () => {
               </ul>
             </div>
           </div>
-          <div tabIndex={11} className="collapse collapse-arrow border-none">
-            <div className="collapse-title text-md font-semibold">
+          <div tabIndex={11} className='collapse collapse-arrow border-none'>
+            <div className='collapse-title text-md font-semibold'>
               Payment Gateway Analytics
             </div>
-            <div className="collapse-content">
+            <div className='collapse-content'>
               <ul>
                 <li>Payment methods used</li>
                 <li>Transaction success rates</li>
@@ -158,11 +158,11 @@ const Sidebar = () => {
               </ul>
             </div>
           </div>
-          <div tabIndex={12} className="collapse collapse-arrow border-none">
-            <div className="collapse-title text-md font-semibold">
+          <div tabIndex={12} className='collapse collapse-arrow border-none'>
+            <div className='collapse-title text-md font-semibold'>
               Marketing Analytics
             </div>
-            <div className="collapse-content">
+            <div className='collapse-content'>
               <ul>
                 <li>Traffic sources</li>
                 <li>Conversion sources</li>
@@ -170,11 +170,11 @@ const Sidebar = () => {
               </ul>
             </div>
           </div>
-          <div tabIndex={13} className="collapse collapse-arrow border-none">
-            <div className="collapse-title text-md font-semibold">
+          <div tabIndex={13} className='collapse collapse-arrow border-none'>
+            <div className='collapse-title text-md font-semibold'>
               Customization and Insights
             </div>
-            <div className="collapse-content">
+            <div className='collapse-content'>
               <ul>
                 <li>Dashboard customization options</li>
                 <li>Data export and reporting</li>
@@ -182,9 +182,9 @@ const Sidebar = () => {
               </ul>
             </div>
           </div>
-          <div tabIndex={14} className="collapse collapse-arrow border-none">
-            <div className="collapse-title text-md font-semibold">Security</div>
-            <div className="collapse-content">
+          <div tabIndex={14} className='collapse collapse-arrow border-none'>
+            <div className='collapse-title text-md font-semibold'>Security</div>
+            <div className='collapse-content'>
               <ul>
                 <li>Security alerts</li>
                 <li>Login attempt history</li>

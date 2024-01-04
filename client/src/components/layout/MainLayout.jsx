a2101514bac3213d83dd8e20d826a3a1f5551e54
ikhtiarbabor <ikhtiarbabor@gmail.com>
2024-01-04 08:08:19 +0600

    merge resolved

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -3,25 +3,15 @@ import Topbar from '../Topbar/Topbar';
 
 import {} from '../ui/resizable';
 
-export function MainLayout({ children, resize, setResize }) {
-  console.log({ resize, setResize });
-  // useEffect(() => {
-  //   const resizeDiv = document.querySelector('.resizeAblePanel');
-  //   resizeDiv.addEventListener('', () => {
-  //     const resizeValue = resizeDiv.getAttribute('data-panel-size');
-  //     setResize(resizeValue);
-  //   });
-  //   console.log(resizeDiv);
-  // }, [setResize]);
-  // console.log(resize);
+export function MainLayout({ children }) {
   return (
     <>
       {/* resize */}
       <Topbar />
       <div className='flex'>
         <Sidebar />
-        <div className='flex items-center justify-center p-6 w-full rounded-t-lg'>
-          <span className='font-semibold'>{children}</span>
+        <div className='flex items-center justify-center p-6 w-full rounded-t-lg border'>
+          <span className='font-semibold border'>{children}</span>
         </div>
       </div>
     </>

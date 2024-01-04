e08224a19d7e5f7e9c9922d66ee4551dfd433ff2
jubair-jx <jubair.official97@gmail.com>
2024-01-04 09:52:23 +0600

    Sidebar icon added

--- client/src/components/Sidebar/Data.jsx
+++ client/src/components/Sidebar/Data.jsx
@@ -1,65 +1,73 @@
-import { BsBag, BsBoxSeam, BsCart3, BsCashCoin } from 'react-icons/bs';
+import { BsBag, BsCashCoin, BsCartPlus } from "react-icons/bs";
+import { LuUserCog2 } from "react-icons/lu";
+import { GrDocumentPerformance } from "react-icons/gr";
+import { LiaShippingFastSolid } from "react-icons/lia";
+import { MdOutlineInventory2 } from "react-icons/md";
+import { RiRefund2Fill } from "react-icons/ri";
+import { FaUsersCog } from "react-icons/fa";
+import { MdOutlinePayments } from "react-icons/md";
+import { FaUserLock } from "react-icons/fa";
 
 export const Data = [
   {
     id: 1,
     icon: <BsBag />,
-    section: 'Order Management',
+    section: "Order Management",
   },
   {
     id: 2,
-    icon: <BsBoxSeam />,
-    section: 'Customer Insights',
+    icon: <LuUserCog2 />,
+    section: "Customer Insights",
   },
   {
     id: 3,
-    icon: <BsCart3 />,
+    icon: <GrDocumentPerformance />,
     submenu: true,
-    section: 'Product Performance',
+    section: "Product Performance",
 
-    subSection: ['Product-wise sales trends', 'Inventory status'],
+    subSection: ["Product-wise sales trends", "Inventory status"],
   },
   {
     id: 4,
-    icon: <BsCashCoin />,
-    section: 'Add Product',
+    icon: <BsCartPlus />,
+    section: "Add Product",
   },
   {
     id: 5,
-    icon: <BsBag />,
-    section: 'Revenue Analytics',
+    icon: <BsCashCoin />,
+    section: "Revenue Analytics",
   },
   {
     id: 6,
-    icon: <BsBag />,
-    section: 'Shipping and Tax',
+    icon: <LiaShippingFastSolid />,
+    section: "Shipping and Tax",
   },
 
   {
     id: 7,
-    icon: <BsBag />,
-    section: 'Stock and Inventory Management',
+    icon: <MdOutlineInventory2 />,
+    section: "Stock and Inventory Management",
   },
   {
     id: 8,
-    icon: <BsBag />,
-    section: 'Refund and Returns',
+    icon: <RiRefund2Fill />,
+    section: "Refund and Returns",
   },
 
   {
     id: 9,
-    icon: <BsBag />,
-    section: 'User Management',
+    icon: <FaUsersCog />,
+    section: "User Management",
   },
   {
     id: 10,
-    icon: <BsBag />,
-    section: 'Payment Gateway Analytics',
+    icon: <MdOutlinePayments />,
+    section: "Payment Gateway Analytics",
   },
 
   {
     id: 11,
-    icon: <BsBag />,
-    section: 'Security',
+    icon: <FaUserLock />,
+    section: "Security",
   },
 ];

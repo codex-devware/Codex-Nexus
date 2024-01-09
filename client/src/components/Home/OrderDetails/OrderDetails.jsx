78c6c732f0ed634749a597bac9356132ae1dd2ff
KawsarCoder <md.kawsar.developer@gmail.com>
2024-01-09 09:19:52 +0600

    line graph check

--- client/src/components/Home/OrderDetails/OrderDetails.jsx
+++ client/src/components/Home/OrderDetails/OrderDetails.jsx
@@ -1,6 +1,85 @@
+import ReactApexChart from "react-apexcharts";
 import expressIcon from "../../../assets/express-delivery.png";
+import { useState } from "react";
 
 const OrderDetails = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [chartState, setChartState] = useState({
+    series: [
+      {
+        name: "XYZ MOTORS",
+        data: [
+          [1609459200000, 40],
+          [1609545600000, 45],
+          [1609632000000, 35],
+          [1609718400000, 50],
+          [1609804800000, 55],
+          [1609891200000, 48],
+          [1609977600000, 52],
+          [1610064000000, 60],
+          [1610150400000, 58],
+          [1610236800000, 55],
+        ],
+      },
+    ],
+    options: {
+      chart: {
+        type: "area",
+        stacked: false,
+        height: 350,
+        zoom: {
+          type: "x",
+          enabled: true,
+          autoScaleYaxis: true,
+        },
+        toolbar: {
+          autoSelected: "zoom",
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      markers: {
+        size: 0,
+      },
+      title: {
+        text: "Stock Price Movement",
+        align: "left",
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          shadeIntensity: 1,
+          inverseColors: false,
+          opacityFrom: 0.5,
+          opacityTo: 0,
+          stops: [0, 90, 100],
+        },
+      },
+      yaxis: {
+        labels: {
+          formatter: function (val) {
+            return (val / 1000000).toFixed(0);
+          },
+        },
+        title: {
+          text: "Price",
+        },
+      },
+      xaxis: {
+        type: "datetime",
+      },
+      tooltip: {
+        shared: false,
+        y: {
+          formatter: function (val) {
+            return (val / 1000000).toFixed(0);
+          },
+        },
+      },
+    },
+  });
+
   return (
     <section className="p-6">
       {/* order details top bar  */}
@@ -173,6 +252,16 @@ const OrderDetails = () => {
             </ul>
           </div>
         </div>
+      </div>
+
+      {/* extra chart  */}
+      <div id="chart">
+        <ReactApexChart
+          options={chartState.options}
+          series={chartState.series}
+          type="area"
+          height={350}
+        />
       </div>
     </section>
   );

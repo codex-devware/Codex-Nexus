ac3ccfe7c59b0c37f2b1e98ccc72bec49fc63fb6
sourovdey <developer.sourovdey@gmail.com>
2024-01-10 12:02:49 +0600

    Activity Done

--- client/src/components/User Management/ActivityBody.jsx
+++ client/src/components/User Management/ActivityBody.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import { MdOutlineShoppingBag } from "react-icons/md";
+import { MdManageAccounts } from "react-icons/md";
 import { FaCircleDot } from "react-icons/fa6";
 
 const ActivityBody = () => {
+
+    const saleData = [
+    {
+        title: 'New Sale',
+        color: '#ff5959',
+        icon: <MdOutlineShoppingBag fontSize={20} />,
+        time: '5 Min Ago',
+        message: 'Exciting news! Customer John Doe just made a purchase. Order #12345 has been successfully placed',
+    },
+    {
+        title: 'New Account Activity',
+        color: '#616161',
+        icon: <MdManageAccounts fontSize={20} />,
+        time: '10 Min Ago',
+        message: 'New account activity detected. User Jane Smith has registered successfully.',
+    },
+];
   return (
     <>
         <section className='font-outfit'>
@@ -14,70 +32,30 @@ const ActivityBody = () => {
             <div className="w-full">
                 
                 <div className="flex flex-col gap-2">
-                    <div className="flex items-center justify-between bg-[#f0f0f0] p-3 rounded-xl">
+                    {saleData.map((item, index) => (
+                        <div key={index} className="flex items-center justify-between bg-[#f0f0f0] p-3 rounded-xl">
                         <div className="flex items-center gap-3">
-                            <div className="bg-[#ff5959] p-3 rounded-full">
-                                <span className='text-white '><MdOutlineShoppingBag fontSize={20}/></span>
+                            <div className={`bg-[${item.color}] p-3 rounded-full`}>
+                                <span className='text-white '>{item.icon}</span>
                             </div>
                             <div className="">
                                 <div className="flex items-center gap-3">
-                                    <span className='text-base font-semibold'>New Sale</span>
+                                    <span className='text-base font-semibold'>{item.title}</span>
                                     <span className="flex items-center gap-1">
                                         <span className='p-1 rounded-full bg-[#444444]'></span>
-                                        <span className='text-xs'>5 Min Ago</span>
+                                        <span className='text-xs'>{item.time}</span>
                                     </span>
                                 </div>
-                                <span>Exciting news! Customer John Doe just made a purchase. Order #12345 has been successfully placed</span>
+                                <span>{item.message}</span>
                             </div>
                         </div>
                         <div className="">
-                            <span className='text-[#ff5959]'><FaCircleDot/></span>
+                            <span className={`text-[${item.color}]`}><FaCircleDot/></span>
                         </div>
                     </div>
-
-
-                    <div className="flex items-center justify-between bg-[#f0f0f0] p-3 rounded-xl">
-                        <div className="flex items-center gap-3">
-                            <div className="bg-[#ff5959] p-3 rounded-full">
-                                <span className='text-white '><MdOutlineShoppingBag fontSize={20}/></span>
-                            </div>
-                            <div className="">
-                                <div className="flex items-center gap-3">
-                                    <span className='text-base font-semibold'>New Sale</span>
-                                    <span className="flex items-center gap-1">
-                                        <span className='p-1 rounded-full bg-[#444444]'></span>
-                                        <span className='text-xs'>5 Min Ago</span>
-                                    </span>
-                                </div>
-                                <span>Exciting news! Customer John Doe just made a purchase. Order #12345 has been successfully placed</span>
-                            </div>
-                        </div>
-                        <div className="">
-                            <span className='text-[#ff5959]'><FaCircleDot/></span>
-                        </div>
-                    </div>
-
-
-                    <div className="flex items-center justify-between bg-[#f0f0f0] p-3 rounded-xl">
-                        <div className="flex items-center gap-3">
-                            <div className="bg-[#ff5959] p-3 rounded-full">
-                                <span className='text-white '><MdOutlineShoppingBag fontSize={20}/></span>
-                            </div>
-                            <div className="">
-                                <div className="flex items-center gap-3">
-                                    <span className='text-base font-semibold'>New Sale</span>
-                                    <span className="flex items-center gap-1">
-                                        <span className='p-1 rounded-full bg-[#444444]'></span>
-                                        <span className='text-xs'>5 Min Ago</span>
-                                    </span>
-                                </div>
-                                <span>Exciting news! Customer John Doe just made a purchase. Order #12345 has been successfully placed</span>
-                            </div>
-                        </div>
-                        <div className="">
-                            <span className='text-[#ff5959]'><FaCircleDot/></span>
-                        </div>
-                    </div>
+          
+                ))}
+                    
                 </div>
             </div>
         </section>

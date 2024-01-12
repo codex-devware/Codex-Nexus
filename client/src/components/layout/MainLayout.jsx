53052dafdf988bf00901e045987a7438af7155de
ikhtiarbabor <ikhtiarbabor@gmail.com>
2024-01-12 07:52:34 +0600

    change main layout

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,20 +1,17 @@
-import { useState } from "react";
-import Sidebar from "../Sidebar/Sidebar";
-import Topbar from "../Topbar/Topbar";
+import { useState } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Topbar from '../Topbar/Topbar';
 
 export function MainLayout({ children }) {
   const [open, setOpen] = useState(true);
-  console.log(open);
   return (
     <>
       {/* resize */}
 
-      <Topbar openToggle={open} setOpenToggle={setOpen} />
-
-      <div className="grid grid-cols-12 justify-center">
+      <div className='grid grid-cols-12 justify-center'>
         <div
           className={`fixed z-50 md:relative ${
-            open ? "lg:col-span-2 md:col-span-3" : "md:col-span-1"
+            open ? 'lg:col-span-2 md:col-span-3' : 'md:col-span-1'
           }`}
         >
           <Sidebar setOpen={setOpen} open={open} />
@@ -22,11 +19,12 @@ export function MainLayout({ children }) {
         <div
           className={`${
             open
-              ? "lg:col-span-10 md:col-span-9 col-span-12"
-              : "col-span-12 md:col-span-11"
+              ? 'lg:col-span-10 md:col-span-9 col-span-12'
+              : 'col-span-12 md:col-span-11'
           }`}
         >
-          <div className="rounded-t-lg border xl:px-12 bg-gray-50">
+          <Topbar openToggle={open} setOpenToggle={setOpen} />
+          <div className='rounded-t-lg border xl:px-12 bg-gray-50'>
             {children}
           </div>
         </div>

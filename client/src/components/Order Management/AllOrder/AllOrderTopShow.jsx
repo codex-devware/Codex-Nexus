a091710cdde5df4320fd52e8fe56935d44675ac1
ikhtiarbabor <ikhtiarbabor@gmail.com>
2024-01-13 10:59:38 +0600

    topbar orgnized done

--- client/src/components/Order Management/AllOrder/AllOrderTopShow.jsx
+++ client/src/components/Order Management/AllOrder/AllOrderTopShow.jsx
@@ -1,53 +1,53 @@
 const AllOrderTopShow = ({ data }) => {
   const { statName, statCount, progress, icon, color } = data;
   return (
-    <div className="flex flex-wrap gap-20 justify-between  shadow font-outfit border p-5 rounded-xl hover:translate-y-[-2px] duration-300 ease-in-out cursor-pointer">
-      <div className="flex justify-between  gap-4">
-        <div className="">
-          <div className="bg-[#f1f3ff] p-3 rounded-lg">
-            <span className="text-[#1E40AF]">{icon}</span>
+    <div className='flex flex-wrap gap-20 justify-between  shadow font-outfit border p-5 rounded-xl hover:translate-y-[-2px] duration-300 ease-in-out cursor-pointer'>
+      <div className='flex justify-between  gap-4'>
+        <div className=''>
+          <div className='bg-[#f1f3ff] p-3 rounded-lg'>
+            <span className='text-[#1E40AF]'>{icon}</span>
           </div>
         </div>
-        <div className="">
-          <h1 className="flex flex-wrap items-end gap-1">
-            <h2 className=" font-medium text-2xl">{statCount}</h2>
+        <div className=''>
+          <h1 className='flex flex-wrap items-end gap-1'>
+            <span className=' font-medium text-2xl'>{statCount}</span>
           </h1>
-          <span className="text-[16px]">{statName}</span>
+          <span className='text-[16px]'>{statName}</span>
         </div>
 
-        <div className="relative h-14 w-14 left-6">
+        <div className='relative h-14 w-14 left-6'>
           <svg
-            className="h-full w-full"
-            width="36"
-            height="36"
-            viewBox="0 0 36 36"
-            xmlns="http://www.w3.org/2000/svg"
+            className='h-full w-full'
+            width='36'
+            height='36'
+            viewBox='0 0 36 36'
+            xmlns='http://www.w3.org/2000/svg'
           >
             <circle
-              cx="18"
-              cy="18"
-              r="16"
-              fill="none"
-              className="stroke-current text-gray-200"
-              strokeWidth="2"
+              cx='18'
+              cy='18'
+              r='16'
+              fill='none'
+              className='stroke-current text-gray-200'
+              strokeWidth='2'
             ></circle>
 
-            <g className="origin-center -rotate-90 transform">
+            <g className='origin-center -rotate-90 transform'>
               <circle
-                cx="18"
-                cy="18"
-                r="16"
-                fill="none"
+                cx='18'
+                cy='18'
+                r='16'
+                fill='none'
                 className={`stroke-current ${color}`}
-                strokeWidth="2"
-                strokeDasharray="100"
+                strokeWidth='2'
+                strokeDasharray='100'
                 strokeDashoffset={progress}
               ></circle>
             </g>
           </svg>
 
-          <div className="absolute top-1/2 start-1/2 transform -translate-y-1/2 -translate-x-1/2">
-            <span className="text-center text-sm font-bold text-gray-800 dark:text-white">
+          <div className='absolute top-1/2 start-1/2 transform -translate-y-1/2 -translate-x-1/2'>
+            <span className='text-center text-sm font-bold text-gray-800 dark:text-white'>
               {progress}%
             </span>
           </div>
--- client/src/components/Sales Overveiw/TotalSales.jsx
+++ client/src/components/Sales Overveiw/TotalSales.jsx
@@ -1,30 +1,31 @@
-import { MdOutlineInsertChartOutlined } from "react-icons/md";
-import { LuArrowUpRightFromCircle } from "react-icons/lu";
+import { LuArrowUpRightFromCircle } from 'react-icons/lu';
+import { MdOutlineInsertChartOutlined } from 'react-icons/md';
 
 const TotalSales = () => {
   return (
     <>
       <div>
-        <div className="flex flex-wrap gap-14 font-outfit border p-5 rounded-xl hover:translate-y-[-2px] duration-300 ease-in-out cursor-pointer">
-          <div className="flex flex-wrap gap-4">
-            <div className="">
-              <div className="bg-[#f1f3ff] p-3 rounded-lg">
-                <span className="text-[#1E40AF]">
+        <div className='flex flex-wrap gap-14 font-outfit border p-5 rounded-xl hover:translate-y-[-2px] duration-300 ease-in-out cursor-pointer'>
+          <div className='flex flex-wrap gap-4'>
+            <div className=''>
+              <div className='bg-[#f1f3ff] p-3 rounded-lg'>
+                <span className='text-[#1E40AF]'>
                   <MdOutlineInsertChartOutlined fontSize={23} />
                 </span>
               </div>
             </div>
-            <div className="">
-              <p className="text-[16px]">Total Sales</p>
-              <h1 className="flex flex-wrap items-end gap-1">
-                <span className="text-[#6B7280] text-sm">$</span>
-                <h2 className=" font-medium text-2xl">41,308</h2>
+            <div className=''>
+              <p className='text-[16px]'>Total Sales</p>
+              <h1 className='flex flex-wrap items-end gap-1'>
+                <span className='text-[#6B7280] text-sm'>$</span>
+                <br />
+                <span className=' font-medium text-2xl'>41,308</span>
               </h1>
             </div>
           </div>
-          <div className="flex flex-wrap items-end">
-            <div className="flex flex-wrap items-center border border-[#EE7214] text-[#FF004D] px-2 py-1 rounded-xl text-sm gap-1">
-              <span className="text-[13px]">
+          <div className='flex flex-wrap items-end'>
+            <div className='flex flex-wrap items-center border border-[#EE7214] text-[#FF004D] px-2 py-1 rounded-xl text-sm gap-1'>
+              <span className='text-[13px]'>
                 <LuArrowUpRightFromCircle />
               </span>
               <span>15%</span>

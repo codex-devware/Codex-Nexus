d6b1d1b4080739e9b880076b2711b13df4695fab
MOHAMMAD IKHTIAR UDDIN <ikhtiarbabor@gmail.com>
2024-03-06 05:32:49 +0600

    return management filter working => returnManagement.jsx

--- client/src/components/Order Management/PaymentInfo/PaymentData.jsx
+++ client/src/components/Order Management/PaymentInfo/PaymentData.jsx
@@ -1,81 +1,81 @@
-import InvestmentGraph from "./InvestmentGraph";
-import PaymentCard from "./PaymentCard";
-import PaymentInfo from "./PaymentInfo";
-import PaymentInfoGraph from "./PaymentInfoGraph";
+import InvestmentGraph from './InvestmentGraph';
+import PaymentCard from './PaymentCard';
+import PaymentInfo from './PaymentInfo';
+import PaymentInfoGraph from './PaymentInfoGraph';
 
 const PaymentData = () => {
   const balanceData = [
     {
       id: 1,
-      title: "Last Balance",
-      balance: "350$",
+      title: 'Last Balance',
+      balance: '350$',
       icon: (
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24"
-          width="24"
-          viewBox="0 0 512 512"
-          fill="#1652F0"
+          xmlns='http://www.w3.org/2000/svg'
+          height='24'
+          width='24'
+          viewBox='0 0 512 512'
+          fill='#1652F0'
         >
-          <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
+          <path d='M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z' />
         </svg>
       ),
-      bColor: "#1652F0",
+      bColor: '#1652F0',
     },
     {
       id: 2,
-      title: "Hold Balance",
-      balance: "350$",
+      title: 'Hold Balance',
+      balance: '350$',
       icon: (
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24"
-          width="24"
-          viewBox="0 0 512 512"
-          fill="#F73164"
+          xmlns='http://www.w3.org/2000/svg'
+          height='24'
+          width='24'
+          viewBox='0 0 512 512'
+          fill='#F73164'
         >
-          <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
+          <path d='M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z' />
         </svg>
       ),
     },
     {
       id: 3,
-      title: "Current Balance",
-      balance: "350$",
+      title: 'Current Balance',
+      balance: '350$',
       icon: (
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24"
-          width="24"
-          viewBox="0 0 512 512"
-          fill="#51BB25"
+          xmlns='http://www.w3.org/2000/svg'
+          height='24'
+          width='24'
+          viewBox='0 0 512 512'
+          fill='#51BB25'
         >
-          <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
+          <path d='M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z' />
         </svg>
       ),
     },
     {
       id: 4,
-      title: "Bounce Balance",
-      balance: "350$",
+      title: 'Bounce Balance',
+      balance: '350$',
       icon: (
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24"
-          width="24"
-          viewBox="0 0 512 512"
-          fill="#DC3545"
+          xmlns='http://www.w3.org/2000/svg'
+          height='24'
+          width='24'
+          viewBox='0 0 512 512'
+          fill='#DC3545'
         >
-          <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
+          <path d='M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z' />
         </svg>
       ),
     },
   ];
   return (
     <>
-      <section className="grid lg:grid-cols-2">
-        <div className="">
-          <div className="grid grid-cols-2 gap-y-6 my-10 justify-items-center">
+      <section className='grid lg:grid-cols-2'>
+        <div className=''>
+          <div className='grid sm:grid-cols-2 gap-6 my-10 justify-items-center'>
             {balanceData?.map((info) => (
               <PaymentInfo key={info.id} info={info} />
             ))}
@@ -85,7 +85,7 @@ const PaymentData = () => {
           <PaymentInfoGraph />
         </div>
       </section>
-      <section className="grid lg:grid-cols-2">
+      <section className='grid lg:grid-cols-2'>
         <InvestmentGraph />
         <PaymentCard />
       </section>
--- client/src/components/Order Management/PaymentInfo/PaymentInfo.jsx
+++ client/src/components/Order Management/PaymentInfo/PaymentInfo.jsx
@@ -3,17 +3,17 @@ const PaymentInfo = ({ info }) => {
 
   return (
     <>
-      <section className="flex flex-wrap w-[268px] flex-col font-outfit border p-5 rounded-xl hover:translate-y-[-2px] duration-300 ease-in-out cursor-pointer">
-        <div className="grid grid-cols-5 flex-col gap-4 ">
-          <div className="rounded-lg col-span-2 flex justify-center items-center ">
-            <span className="bg-white p-3 rounded-full">{icon}</span>
+      <section className='w-full border p-5 py-8  rounded-xl hover:translate-y-[-2px] duration-300 ease-in-out cursor-pointer'>
+        <div className='flex gap-4  justify-center'>
+          <div className='rounded-lg flex justify-center items-center '>
+            <span className='bg-white p-3 rounded-full'>{icon}</span>
           </div>
-          <div className="col-span-3">
-            <p className="text-[18px] font-semi-bold">{title}</p>
-            <h1 className=" gap-10  ">
-              <span className=" font-medium text-2xl">{balance}</span>
+          <article>
+            <p className='text-[18px] font-semi-bold'>{title}</p>
+            <h1 className=' gap-10  '>
+              <span className=' font-medium text-2xl'>{balance}</span>
             </h1>
-          </div>
+          </article>
         </div>
       </section>
     </>
